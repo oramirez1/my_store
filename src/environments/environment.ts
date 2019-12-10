41e7b7cd48fe64e3cb97aa1f745008b8a9ccb67c
Javier Ramirez <javier.ramirez@gmail.com>
2019-12-10 10:46:05 -0500

    Changed Webservices Path to our own hosted webservices

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:3000/v1'
+  //baseUrl: 'http://localhost:3000/v1'
+  baseUrl: 'http://glass.eis.utoronto.ca:3000/v1'
   //baseUrl: 'http://ec2-3-134-92-53.us-east-2.compute.amazonaws.com:8080/products-ut-wo-db/rest/'
 };
 
